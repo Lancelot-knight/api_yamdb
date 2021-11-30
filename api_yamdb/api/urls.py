6e1b1116b0e3acdead5e1af843a372f0b286082f
Aleksey <nightwolfero@yandex.ru>
2021-11-30 17:01:09 +0300

    Add views, permissions and urls from feature_2

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
+from . import views
+
+router_v1 = DefaultRouter()
+router_v1.register('titles', views.TitleViewSet, basename='title')
+router_v1.register('categories', views.CategoryViewSet, basename='category')
+router_v1.register('genres', views.GenreViewSet, basename='genre')
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.db.models import Avg
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, viewsets
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from .filters import TitleFilter
+from .permissions import IsAdminUserOrReadOnly
+from reviews.models import Category, Genre, Title
+from .serializers import (CategorySerializer,
+                          GenreSerializer,
+                          TitleReadSerializer, TitleWriteSerializer,
+                          )
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.annotate(rating=Avg('reviews__score'))
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.request.method in ['POST', 'PATCH']:
+            return TitleWriteSerializer
+        return TitleReadSerializer
+
+
+class MixinSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
+    pass
+
+
+class CategoryViewSet(MixinSet):
+    queryset = Category.objects.all().order_by('id')
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=name']
+    lookup_field = 'slug'
+
+
+class GenreViewSet(MixinSet):
+    queryset = Genre.objects.all().order_by('id')
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=name']
+    lookup_field = 'slug'
